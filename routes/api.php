689b99a31ffd76379b12a0c4518ab6bbe1a6c150
xwen1000 <xingwen1000@126.com>
2020-10-02 06:51:18 +0000

    用户信息

--- routes/api.php
+++ routes/api.php
@@ -52,5 +52,14 @@ Route::prefix('v1')
         // 删除token
         Route::delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('authorizations.destroy');
+        // 某个用户的详情
+        Route::get('users/{user}', 'UsersController@show')
+            ->name('users.show');
+        // 登录后可以访问的接口
+        Route::middleware('auth:api')->group(function() {
+            // 当前登录用户信息
+            Route::get('user', 'UsersController@me')
+                ->name('user.show');
+     });
     
 });
