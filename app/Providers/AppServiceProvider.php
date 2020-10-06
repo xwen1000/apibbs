7cee691fecfab67e0d7e5d1bb3c0830fcef5ec19
xwen1000 <xingwen1000@126.com>
2020-10-06 23:45:45 +0000

    生成话题骨架

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+
         //
     }
 }
